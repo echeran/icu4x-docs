8ee738a6804318365662d0cf1bfca5f08cfbb9b6
echeran <echeran@users.noreply.github.com>
2020-08-05 03:24:40 +0000

    deploy: echeran/icu4x@59f74965ef1600c7fccf6c9aec4a785ce842af86

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1596596813594,
+  "lastUpdate": 1596597878456,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13,126 +13,126 @@ window.BENCHMARK_DATA = {
             "name": "echeran",
             "username": "echeran"
           },
-          "id": "10b652277f6373eae761a15345d55cf0c487fb76",
+          "id": "83373a8bc06d15c1b7bf2d481f4168f48e6f5286",
           "message": "Testing PR to test GH Actions CI for runtime perf benchmarking",
           "timestamp": "2020-07-27T21:18:27Z",
-          "url": "https://github.com/echeran/icu4x/pull/4/commits/10b652277f6373eae761a15345d55cf0c487fb76"
+          "url": "https://github.com/echeran/icu4x/pull/4/commits/83373a8bc06d15c1b7bf2d481f4168f48e6f5286"
         },
-        "date": 1596596811366,
+        "date": 1596597876622,
         "tool": "cargo",
         "benches": [
           {
             "name": "langid/construct/langid",
-            "value": 782,
-            "range": "± 65",
+            "value": 702,
+            "range": "± 20",
             "unit": "ns/iter"
           },
           {
             "name": "langid/construct/locale",
-            "value": 1254,
-            "range": "± 89",
+            "value": 1192,
+            "range": "± 28",
             "unit": "ns/iter"
           },
           {
             "name": "langid/to_string/langid",
-            "value": 2020,
-            "range": "± 156",
+            "value": 2066,
+            "range": "± 64",
             "unit": "ns/iter"
           },
           {
             "name": "langid/to_string/locale",
-            "value": 2013,
-            "range": "± 54",
+            "value": 2072,
+            "range": "± 47",
             "unit": "ns/iter"
           },
           {
             "name": "langid/compare/struct/langid",
-            "value": 18,
-            "range": "± 1",
+            "value": 15,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
             "name": "langid/compare/struct/locale",
-            "value": 18,
-            "range": "± 1",
+            "value": 15,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
             "name": "langid/compare/str/langid",
-            "value": 2287,
-            "range": "± 112",
+            "value": 2292,
+            "range": "± 52",
             "unit": "ns/iter"
           },
           {
             "name": "langid/compare/str/locale",
-            "value": 2294,
-            "range": "± 166",
+            "value": 2274,
+            "range": "± 64",
             "unit": "ns/iter"
           },
           {
             "name": "langid/canonicalize/langid",
-            "value": 3823,
-            "range": "± 412",
+            "value": 3979,
+            "range": "± 116",
             "unit": "ns/iter"
           },
           {
             "name": "langid/canonicalize/locale",
-            "value": 5186,
-            "range": "± 416",
+            "value": 5105,
+            "range": "± 122",
             "unit": "ns/iter"
           },
           {
             "name": "locale/construct/locale",
-            "value": 2370,
-            "range": "± 146",
+            "value": 2374,
+            "range": "± 78",
             "unit": "ns/iter"
           },
           {
             "name": "locale/to_string/locale",
-            "value": 3206,
-            "range": "± 281",
+            "value": 3325,
+            "range": "± 79",
             "unit": "ns/iter"
           },
           {
             "name": "locale/compare/struct/locale",
-            "value": 118,
-            "range": "± 4",
+            "value": 119,
+            "range": "± 2",
             "unit": "ns/iter"
           },
           {
             "name": "locale/compare/str/locale",
-            "value": 3233,
-            "range": "± 373",
+            "value": 3541,
+            "range": "± 261",
             "unit": "ns/iter"
           },
           {
             "name": "locale/canonicalize/locale",
-            "value": 6611,
-            "range": "± 499",
+            "value": 6744,
+            "range": "± 173",
             "unit": "ns/iter"
           },
           {
             "name": "language_subtag_parse",
-            "value": 341,
-            "range": "± 12",
+            "value": 328,
+            "range": "± 13",
             "unit": "ns/iter"
           },
           {
             "name": "script_subtag_parse",
-            "value": 112,
+            "value": 129,
             "range": "± 3",
             "unit": "ns/iter"
           },
           {
             "name": "region_subtag_parse",
-            "value": 128,
-            "range": "± 2",
+            "value": 126,
+            "range": "± 1",
             "unit": "ns/iter"
           },
           {
             "name": "variant_subtag_parse",
-            "value": 274,
-            "range": "± 17",
+            "value": 257,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]
