420e28030c0fce3ea5afd33b3d24745a9d218b59
echeran <echeran@users.noreply.github.com>
2020-10-08 22:32:51 +0000

    Bench Dash (runtime, components/locale) -> GH Pages echeran/icu4x@e70599149597a3f63ed76570f18145d48d650a2d

--- dev/components/locale/data.js
+++ dev/components/locale/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602188642713,
+  "lastUpdate": 1602196368007,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13,42 +13,42 @@ window.BENCHMARK_DATA = {
             "name": "echeran",
             "username": "echeran"
           },
-          "id": "d0c0ff75ba818e0380cb356ccfdf445bb369d793",
+          "id": "7e9af8b8e59dfa99a8d8ff21317f1557fcb7f3f4",
           "message": "Test PR on personal fork - API docs/CI dashboard",
           "timestamp": "2020-10-06T19:21:31Z",
-          "url": "https://github.com/echeran/icu4x/pull/10/commits/d0c0ff75ba818e0380cb356ccfdf445bb369d793"
+          "url": "https://github.com/echeran/icu4x/pull/10/commits/7e9af8b8e59dfa99a8d8ff21317f1557fcb7f3f4"
         },
-        "date": 1602188640307,
+        "date": 1602196365581,
         "tool": "cargo",
         "benches": [
           {
             "name": "isize/smaller",
-            "value": 29156,
-            "range": "± 2679",
+            "value": 34785,
+            "range": "± 2173",
             "unit": "ns/iter"
           },
           {
             "name": "isize/larger",
-            "value": 67689,
-            "range": "± 6748",
+            "value": 80843,
+            "range": "± 5124",
             "unit": "ns/iter"
           },
           {
             "name": "to_string/908070605040302010",
-            "value": 263,
-            "range": "± 30",
+            "value": 293,
+            "range": "± 25",
             "unit": "ns/iter"
           },
           {
             "name": "write_to/908070605040302010",
-            "value": 126,
-            "range": "± 11",
+            "value": 140,
+            "range": "± 8",
             "unit": "ns/iter"
           },
           {
             "name": "from_string/1000000001",
-            "value": 79,
-            "range": "± 6",
+            "value": 88,
+            "range": "± 4",
             "unit": "ns/iter"
           }
         ]
