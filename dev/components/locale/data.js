1f50602cab43b6de12def9034c3c0760823343b4
echeran <echeran@users.noreply.github.com>
2020-10-09 02:33:05 +0000

    Rust doc -> GH Pages echeran/icu4x@67823919fe134995b484017eb6f0e8308b50de3f

--- dev/components/locale/data.js
+++ dev/components/locale/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602209868908,
+  "lastUpdate": 1602210654613,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -49,6 +49,56 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 83,
             "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "echeran",
+            "username": "echeran"
+          },
+          "committer": {
+            "name": "echeran",
+            "username": "echeran"
+          },
+          "id": "4c1fd4862a6156559d5c5860e7d8be228d382fac",
+          "message": "Test PR on personal fork - API docs/CI dashboard",
+          "timestamp": "2020-10-06T19:21:31Z",
+          "url": "https://github.com/echeran/icu4x/pull/10/commits/4c1fd4862a6156559d5c5860e7d8be228d382fac"
+        },
+        "date": 1602210652876,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "isize/smaller",
+            "value": 28720,
+            "range": "± 2433",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 68868,
+            "range": "± 4238",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/908070605040302010",
+            "value": 254,
+            "range": "± 18",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_to/908070605040302010",
+            "value": 118,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 79,
+            "range": "± 6",
             "unit": "ns/iter"
           }
         ]
