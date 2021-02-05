4b5bf41f91135285006b2da349522c5b71edb9ad
echeran <echeran@users.noreply.github.com>
2021-02-05 05:48:10 +0000

    Rust API docs + benchmark dashboards -> GH Pages echeran/icu4x@03b3486395ee1caa8970cc972c46c2ec40cd8ec7

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602734885569,
+  "lastUpdate": 1612503910215,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -59,6 +59,246 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 1029700,
             "range": "± 2751",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fbce283a133bf8865ded6c2806cd992a201a82db",
+          "message": "Merge pull request #366 from unicode-org/mihai_latin1\n\nFix for issue #357, Latin 1 definition",
+          "timestamp": "2020-10-26T02:56:13-07:00",
+          "tree_id": "c000495ad2a67b40a7f97be0e3058e31901c02b1",
+          "url": "https://github.com/echeran/icu4x/commit/fbce283a133bf8865ded6c2806cd992a201a82db"
+        },
+        "date": 1604359870863,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 881903,
+            "range": "± 75792",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "73f1d954dc2316e425014a4c68b0339f34ef9d47",
+          "message": "Use parsing iterator for LanguageIdentifier PartialEq (#386)\n\n* Use parsing iterator for LanguageIdentifier PartialEq\r\n\r\n* Address review feedback",
+          "timestamp": "2020-11-10T10:46:03-08:00",
+          "tree_id": "b19692744ffbf0a320c983dd6a0b6cb5405cbdf4",
+          "url": "https://github.com/echeran/icu4x/commit/73f1d954dc2316e425014a4c68b0339f34ef9d47"
+        },
+        "date": 1605201734853,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 806577,
+            "range": "± 10399",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "bd0d43a9383201acd02e13cba79c54856a729026",
+          "message": "Reorganize docs and add wiki articles. (#382)",
+          "timestamp": "2020-11-12T12:38:17-06:00",
+          "tree_id": "ffb79c9115631c738f6188bcaf7225b879839492",
+          "url": "https://github.com/echeran/icu4x/commit/bd0d43a9383201acd02e13cba79c54856a729026"
+        },
+        "date": 1605232494654,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 772354,
+            "range": "± 62412",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7033c6bd7dd2c42313b73b6be295f0c0b1092fa4",
+          "message": "Merge pull request #19 from echeran/codecov\n\nTesting PR - change Coveralls -> Codecov",
+          "timestamp": "2020-11-12T19:37:08-08:00",
+          "tree_id": "46ee2ae180afff1a82095c4588cf15e10bab8cc4",
+          "url": "https://github.com/echeran/icu4x/commit/7033c6bd7dd2c42313b73b6be295f0c0b1092fa4"
+        },
+        "date": 1605238819941,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 779686,
+            "range": "± 43339",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4cae8e0a2f1e4d497621055c77d0446e17cbe041",
+          "message": "Populating Writeable readme with lib.rs (#389)",
+          "timestamp": "2020-11-24T17:10:57-06:00",
+          "tree_id": "bc1bfc4b39890657ec5c9bd0d56eadacaca9f587",
+          "url": "https://github.com/echeran/icu4x/commit/4cae8e0a2f1e4d497621055c77d0446e17cbe041"
+        },
+        "date": 1606776719013,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 818388,
+            "range": "± 3594",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9f01c66ca6126c48cc65dc817e3c820cf7d7f78",
+          "message": "Merge pull request #21 from echeran/gregtatum/memory-benchmark-upload-fix\n\nTesting PR on echeran's fork for gregtatum/memory-benchmark-upload-fix",
+          "timestamp": "2021-02-04T21:12:24-08:00",
+          "tree_id": "a7c739d23fce79b570dccb6eed947f4f4351130a",
+          "url": "https://github.com/echeran/icu4x/commit/d9f01c66ca6126c48cc65dc817e3c820cf7d7f78"
+        },
+        "date": 1612502274386,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1132813,
+            "range": "± 66994",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae89da479dfbbcfc73fbaa977cd3f4ecfd0e4fac",
+          "message": "Allow some clippy lints (#474)",
+          "timestamp": "2021-02-02T17:40:53-08:00",
+          "tree_id": "b24a02430c995fda5f39e5982049523f7037643c",
+          "url": "https://github.com/echeran/icu4x/commit/ae89da479dfbbcfc73fbaa977cd3f4ecfd0e4fac"
+        },
+        "date": 1612503335237,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1055331,
+            "range": "± 30649",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03b3486395ee1caa8970cc972c46c2ec40cd8ec7",
+          "message": "Merge pull request #22 from echeran/gregtatum/memory-benchmark-upload-fix\n\nTesting PR on echeran's fork for gregtatum/memory-benchmark-upload-fix",
+          "timestamp": "2021-02-04T21:35:52-08:00",
+          "tree_id": "d21f8c594b7fe2a7e1cff2ff8413b7331c672237",
+          "url": "https://github.com/echeran/icu4x/commit/03b3486395ee1caa8970cc972c46c2ec40cd8ec7"
+        },
+        "date": 1612503909622,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1106972,
+            "range": "± 61544",
             "unit": "ns/iter"
           }
         ]
